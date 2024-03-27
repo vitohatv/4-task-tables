cd6a8f5099c43ae4dc9425a774862a9d7d261373
Vitaliy <vitohatv@gmail.com>
2024-03-28 02:33:22 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import FirstTable from './Containers/FirstTable/FirstTable';
+import SecondTable from './Containers/SecondTable/SecondTable';
+
+function App() {
+  return (
+    <div className="App">
+      <FirstTable  />
+      <SecondTable />
+    </div>
+  );
+}
+export default App;
