3be1b210047edff073977a28cb7af715655346c0
shamiall <abdulabdulika@gmail.com>
2024-03-29 12:16:31 +0400

    fix skeleton

--- src/Containers/FirstTable/FirstTable.jsx
+++ src/Containers/FirstTable/FirstTable.jsx
@@ -1,175 +1,39 @@
-import { Skeleton, Table } from 'antd';
-import { useEffect, useState } from 'react';
-import './FirstTable.css';
+import { Skeleton, Table } from "antd";
+import "./FirstTable.css";
 
 const FirstTable = () => {
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const columns = [
+    {
+      title: <Skeleton.Input active />,
+      dataIndex: "price",
+      key: "price",
+      render: (text) => <Skeleton active paragraph={{ rows: 0 }} />,
+    },
+    {
+      title: <Skeleton.Input active />,
+      dataIndex: "amount",
+      key: "amount",
+      render: (text) => <Skeleton active paragraph={{ rows: 0 }} />,
+    },
+    {
+      title: <Skeleton.Input active />,
+      dataIndex: "total",
+      key: "total",
+      render: (text) => <Skeleton active paragraph={{ rows: 0 }} />,
+    },
+  ];
 
-    useEffect(() => {
-        const tableData = () => {
-            setTimeout(() => {
-                setData([
-                    {
-                      key: '1',
-                      price: 71612.5,
-                      amount: 6000,
-                      total: 175660
-                    },
-                    {
-                      key: '2',
-                      price: 71610,
-                      amount: 75000,
-                      total: 169660
-                    },
-                    {
-                      key: '3',
-                      price: 71605,
-                      amount: 6000,
-                      total: 94660
-                    },
-                    {
-                      key: '4',
-                      price: 71590,
-                      amount: 60,
-                      total: 88660
-                    },
-                    {
-                      key: '5',
-                      price: 71585,
-                      amount: 46000,
-                      total: 88600
-                    },
-                    {
-                      key: '6',
-                      price: 71582.5,
-                      amount: 4000,
-                      total: 42600
-                    },
-                    {
-                      key: '7',
-                      price: 71575,
-                      amount: 6000,
-                      total: 38600
-                    },
-                    {
-                      key: '8',
-                      price: 71572.5,
-                      amount: 14000,
-                      total: 32600
-                    },
-                    {
-                      key: '9',
-                      price: 71570,
-                      amount: 6600,
-                      total: 18600
-                    },
-                    {
-                      key: '10',
-                      price: 71567.5,
-                      amount: 12000,
-                      total: 12000
-                    },
-                    {
-                      key: '11',
-                      price: 71540.5,
-                      amount: 6000,
-                      total: 175660
-                    },
-                    {
-                      key: '12',
-                      price: 71535,
-                      amount: 75000,
-                      total: 169660
-                    },
-                    {
-                      key: '13',
-                      price: 71532,
-                      amount: 6000,
-                      total: 94660
-                    },
-                    {
-                      key: '14',
-                      price: 71525,
-                      amount: 60,
-                      total: 88660
-                    },
-                    {
-                      key: '15',
-                      price: 71522.5,
-                      amount: 6200,
-                      total: 88600
-                    },
-                    {
-                      key: '16',
-                      price: 71515,
-                      amount: 1800,
-                      total: 42600
-                    },
-                    {
-                      key: '17',
-                      price: 71512.5,
-                      amount: 15610,
-                      total: 38600
-                    },
-                    {
-                      key: '18',
-                      price: 71505,
-                      amount: 6010,
-                      total: 32600
-                    },
-                    {
-                      key: '19',
-                      price: 71497.5,
-                      amount: 6600,
-                      total: 18600
-                    },
-                    {
-                      key: '20',
-                      price: 71495,
-                      amount: 40000,
-                      total: 12000
-                    }
-                ]);
-                setLoading(false);
-            }, 2500);
-        };
-
-        tableData();
-    }, []);
-
-    const columns = [
-        {
-            title: loading ? <Skeleton.Input active /> : 'Price',
-            dataIndex: 'price',
-            key: 'price',
-            render: text => loading ? <Skeleton active paragraph={{ rows: 0 }} /> : text 
-        },
-        {
-            title: loading ? <Skeleton.Input active /> : 'Amount',
-            dataIndex: 'amount',
-            key: 'amount',
-            render: text => loading ? <Skeleton active paragraph={{ rows: 0 }} /> : text
-        },
-        {
-            title: loading ? <Skeleton.Input active /> : 'Total',
-            dataIndex: 'total',
-            key: 'total',
-            render: text => loading ? <Skeleton active paragraph={{ rows: 0 }} /> : text
-        },
-    ];
-
-    return (
-        <div>
-            <Table 
-                dataSource={loading ? [{}] : data} 
-                columns={columns}
-                loading={loading} 
-                pagination={false}
-                scroll={{x: '100%'}}
-            />
-        </div>
-    );
+  return (
+    <div style={{ maxWidth: 600 }}>
+      <Table
+        size="small"
+        dataSource={[{}, {}, {}, {}, {}, {}, {}]}
+        columns={columns}
+        pagination={false}
+        scroll={{ x: "100%" }}
+      />
+    </div>
+  );
 };
 
 export default FirstTable;
--- src/Containers/SecondTable/SecondTable.jsx
+++ src/Containers/SecondTable/SecondTable.jsx
@@ -1,147 +1,42 @@
-import { Button, Skeleton, Table, Tag } from 'antd';
-import Column from 'antd/es/table/Column';
-import { useEffect, useState } from 'react';
-import './SecondTable.css';
+import { Button, Skeleton, Table, Tag } from "antd";
+import Column from "antd/es/table/Column";
+import { useEffect, useState } from "react";
+import "./SecondTable.css";
 
 const SecondTable = () => {
   const [loading, setLoading] = useState(true);
-  const [data, setData] = useState([]); 
+  const [data, setData] = useState([]);
 
-  useEffect(() => {
-    const tableBtcMarket = () => {
-      setTimeout(() => {
-        const data = [
-          {
-            key: '1',
-            price: -952.5,
-            amount: 200,
-            total: '$ 200',
-            products: [
-              { key: '1', nameDeal:'BTC-5APR24',  tags: ['buy'],  amountInTable: 200, limitInTable: '71,567.50',  marketPrice: '71,567.50', },
-              { key: '2', nameDeal:'BTC-26APR24', tags: ['sell'], amountInTable: 200, limitInTable: '72,520.00',  marketPrice: '72,520.00', },
-            ],
-          },
-          {
-            key: '2',
-            price: -947.5,
-            amount: 9780,
-            total: '$ 9 980',
-            products: [
-              { key: '3', nameDeal:'BTC-9APR24',  tags: ['buy'],  amountInTable: 9780, limitInTable: '71,497.84',  marketPrice: '71,507.84', },
-              { key: '4', nameDeal:'BTC-29APR24', tags: ['sell'], amountInTable: 800, limitInTable: '71,550.00',  marketPrice: '71,560.00', },
-            ],
-          },
-          {
-            key: '3',
-            price: -927.5,
-            amount: 600,
-            total: '$ 10 580',
-            products: [
-              { key: '5', nameDeal:'BTC-4APR24',  tags: ['buy'],  amountInTable: 600, limitInTable: '71,497.84',  marketPrice: '71,497.84', },
-              { key: '6', nameDeal:'BTC-17APR24', tags: ['sell'], amountInTable: 200, limitInTable: '71,550.00',  marketPrice: '71,560.00', },
-            ],
-          },
-          {
-            key: '4',
-            price: -922.5,
-            amount: 1420,
-            total: '$ 12 000',
-            products: [
-              { key: '7', nameDeal:'BTC-6APR24',  tags: ['buy'],  amountInTable: 600, limitInTable: '71,497.84',  marketPrice: '71,497.84', },
-              { key: '8', nameDeal:'BTC-16APR24', tags: ['sell'], amountInTable: 200, limitInTable: '71,550.00',  marketPrice: '71,560.00', },
-            ],
-          },
-          {
-            key: '5',
-            price: -920,
-            amount: 6600,
-            total: '$ 18 600',
-            products: [
-              { key: '9', nameDeal:'BTC-8APR24',  tags: ['buy'],  amountInTable: 600, limitInTable: '71,497.84',  marketPrice: '71,497.84', },
-              { key: '10', nameDeal:'BTC-21APR24', tags: ['sell'], amountInTable: 200, limitInTable: '71,550.00',  marketPrice: '71,560.00', },
-            ],
-          },
-          {
-            key: '6',
-            price: -917.5,
-            amount: 14000,
-            total: '$ 32 600',
-            products: [
-              { key: '11', nameDeal:'BTC-8APR24',  tags: ['buy'],  amountInTable: 600, limitInTable: '71,497.84',  marketPrice: '71,497.84', },
-              { key: '12', nameDeal:'BTC-20APR24', tags: ['sell'], amountInTable: 200, limitInTable: '71,550.00',  marketPrice: '71,560.00', },
-            ]
-          }
-        ];
-        setData(data);
-        setLoading(false); 
-      }, 2500);
-    };
-    tableBtcMarket();
-  },[])
-
-
-  const expandedRowRender = (record) => (
-    <div>
-      <Table dataSource={record.products} pagination={false} >
-      <Column dataIndex="nameDeal" key="nameDeal" />
+  return (
+    <div style={{ width: "100%", maxWidth: 900 }}>
+      <Table
+        dataSource={loading ? [{}, {}, {}, {}, {}] : data}
+        pagination={false}
+        size="small"
+        className="table-second"
+        scroll={{ x: "100%" }}
+      >
         <Column
-            title="Side"
-            dataIndex="tags"
-            key="tags"
-            
-            render={(tags) => (
-              <div>
-                {tags.map((tag) => (
-                  <Tag color={tag === 'sell' ? 'volcano' : 'green'} key={tag}>
-                    {tag.toUpperCase()}
-                  </Tag>
-                ))}
-              </div>
-            )}
+          title={<Skeleton.Input active />}
+          dataIndex="price"
+          key="price"
+          render={(text) => <Skeleton active paragraph={{ rows: 0 }} />}
         />
-          
-        <Column title="Amount" dataIndex="amountInTable" key="amountInTable" />
-        <Column title="Limit" dataIndex="limitInTable" key="limitInTable"    />
-        <Column title="Market" dataIndex="marketPrice" key="marketPrice"     />
+        <Column
+          title={<Skeleton.Input active />}
+          dataIndex="amount"
+          key="amount"
+          render={(text) => <Skeleton active paragraph={{ rows: 0 }} />}
+        />
+        <Column
+          title={<Skeleton.Input active />}
+          dataIndex="total"
+          key="total"
+          render={(text) => <Skeleton active paragraph={{ rows: 0 }} />}
+        />
       </Table>
-        <Button type="primary" block className='button-inner-table'>
-          Trade
-        </Button>
     </div>
   );
-
-return (
-  <div>
-    <Table 
-      dataSource={loading ? [{}] : data} 
-      expandable={{ expandedRowRender }} 
-      pagination={false}
-      loading={loading}
-      className='table-second'
-      scroll={{x: '100%'}}
-    >
-
-      <Column 
-        title={loading ? <Skeleton.Input active /> : "Price"}  
-        dataIndex="price"   
-        key="price"    
-        render={text => loading ? <Skeleton active paragraph={{ rows: 1 }} /> : text} 
-      />
-      <Column 
-        title={loading ? <Skeleton.Input active /> : "Amount"} 
-        dataIndex="amount" 
-        key="amount"    
-        render={text => loading ? <Skeleton active paragraph={{ rows: 1 }} /> : text} 
-      />
-      <Column 
-        title={loading ? <Skeleton.Input active /> : "Total"}  
-        dataIndex="total"   
-        key="total"    
-        render={text => loading ? <Skeleton active paragraph={{ rows: 1 }} /> : text} 
-      />
-    </Table>
-  </div>
-  )
-}
+};
 
 export default SecondTable;
