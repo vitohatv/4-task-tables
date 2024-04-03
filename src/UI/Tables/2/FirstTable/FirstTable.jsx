596ff2eacd0efaaaa9f06e4c5ab2804a1684f9f7
shamiall <abdulabdulika@gmail.com>
2024-04-03 14:41:09 +0400

    fix first table

--- src/UI/Tables/2/FirstTable/FirstTable.jsx
+++ src/UI/Tables/2/FirstTable/FirstTable.jsx
@@ -1,60 +1,65 @@
-import { Skeleton, Table } from 'antd';
-import './FirstTable.css';
+import { Skeleton, Table } from "antd";
+import "./FirstTable.css";
 
 const FirstTable = () => {
-  const data = Array.from({length: 20}, (_, i) => ({}));
-  data[9].space = true;
+  const data = Array.from({ length: 11 }, (_, i) => ({}));
+  data[5].space = true;
   const columns = [
     {
-      title: <Skeleton.Input size='small' className='sceleton-title-first' active />,
+      title: (
+        <Skeleton.Input size="small" className="sceleton-title-first" active />
+      ),
       dataIndex: "price",
       key: "price",
+      align: "center",
       render: (text, record, index) => {
         if (record.space) {
           return null;
         } else {
-          return <Skeleton className='sceleton-text-first' active paragraph={{ rows: 0 }} />;
+          return <Skeleton.Button active={true} size="small" />;
         }
       },
     },
     {
-      title: <Skeleton.Input size='small' className='sceleton-title-second' active />,
+      title: (
+        <Skeleton.Input size="small" className="sceleton-title-second" active />
+      ),
       dataIndex: "amount",
       key: "amount",
+      align: "center",
       render: (text, record, index) => {
         if (record.space) {
           return null;
         } else {
-          return <Skeleton className='sceleton-text-second' active paragraph={{ rows: 0 }} />;
+          return <Skeleton.Button active={true} size="small" />;
         }
       },
     },
     {
-      title: <Skeleton.Input 
-      size='small' 
-      className='sceleton-title-third' 
-      active         
-      />,
+      title: (
+        <Skeleton.Input size="small" className="sceleton-title-third" active />
+      ),
       dataIndex: "total",
       key: "total",
+      align: "center",
       render: (text, record, index) => {
         if (record.space) {
           return null;
         } else {
-          return <Skeleton className='sceleton-text-third' active paragraph={{ rows: 0 }} />;
+          return <Skeleton.Button active={true} size="small" />;
         }
       },
     },
   ];
 
-
   return (
-    <div className='first-table-container'>
+    <div className="first-table-container">
       <Table
-        className='table-inner'
+        className="table-inner"
         dataSource={data}
         columns={columns}
-        pagination={false} 
+        pagination={false}
+        size="small"
       />
     </div>
   );
